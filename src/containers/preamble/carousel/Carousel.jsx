8f8e96b99c32b600204778e85951bb1a36944e2c
kdtran <King.Dang.Tran@mgm-tp.com>
2020-03-23 19:20:21 +0700

    Carousel

--- src/containers/preamble/carousel/Carousel.jsx
+++ src/containers/preamble/carousel/Carousel.jsx
@@ -1,24 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ArrowUp from '../../../resources/icon/arrow_up.svg';
+import ArrowDown from '../../../resources/icon/arrow_down.svg';
+import CarouselPics from '../../../resources/picture/carousel';
 import './Carousel.css';
 
+function getExpectCarouselIndex(expectIndex, maxIndex) {
+  if (expectIndex > maxIndex - 1) {
+    return 0;
+  } if (expectIndex < 0) {
+    return maxIndex - 1;
+  }
+  return expectIndex;
+}
+
+function counterFormat(number) {
+  if (number < 10) {
+    return ` 0${number} `;
+  }
+  return number;
+}
+
 export default function Carousel() {
-  const pictureNames = ['carousel_1', 'carousel_2', 'carousel_3', 'carousel_4'];
-  const indicators = pictureNames.map(
-    (pictureName) => (<li className="carousel__indicator-element" key={pictureName} />),
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const indicators = CarouselPics.map(
+    (info, index) => {
+      const id = CarouselPics.length - 1 - index;
+      const boldClass = currentIndex === id ? 'carousel__indicator-element--active' : '';
+      return <li className={`carousel__indicator-element ${boldClass}`} key={info} />;
+    },
   );
   return (
     <div className="carousel">
       <div className="carousel__indicator">
         <div className="carousel__counter">
-          <span className="carousel__counter-index">02 </span>
+          <span className="carousel__counter-index">
+            {counterFormat(currentIndex + 1)}
+          </span>
           <span className="carousel__counter-total">
-            / 0
-            {pictureNames.length}
+            /
+            {counterFormat(CarouselPics.length)}
           </span>
         </div>
         <ul className="carousel__indicator-list">
           {indicators}
         </ul>
+        <div className="carousel__arrow">
+          <button
+            className="carousel__arrow carousel__arrow--up"
+            onClick={
+              () => setCurrentIndex(getExpectCarouselIndex(currentIndex + 1, CarouselPics.length))
+            }
+            type="button"
+          >
+            <img src={ArrowUp} alt="Next" />
+          </button>
+          <button
+            className="carousel__arrow carousel__arrow--down"
+            onClick={
+              () => setCurrentIndex(getExpectCarouselIndex(currentIndex - 1, CarouselPics.length))
+            }
+            type="button"
+          >
+            <img src={ArrowDown} alt="Previous" />
+          </button>
+        </div>
+      </div>
+      <div>
+        <img className="carousel__image" src={CarouselPics[currentIndex]} alt="Test" />
       </div>
     </div>
   );
---
+++ src/resources/picture/carousel/index.jsx
@@ -0,0 +1,11 @@
+import Carousel1 from './carousel_1.jpg';
+import Carousel2 from './carousel_2.jpg';
+import Carousel3 from './carousel_3.jpg';
+import Carousel4 from './carousel_4.jpg';
+
+/**
+ * This file is using to manage and export the image as a dynamic import
+ * for this folder so it can easily get it in source code
+ */
+
+export default [Carousel1, Carousel2, Carousel3, Carousel4];
