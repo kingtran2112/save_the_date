7783a968006c4fff902404c18a4e37294569634a
kdtran <King.Dang.Tran@mgm-tp.com>
2020-03-25 19:26:45 +0700

    Change layout of the page

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import SideBar from './sidebar/SideBar';
 import MainPage from './mainpage/MainPage';
+import PatternLeft from '../resources/picture/pattern/pattern-left.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="app-container">
       <SideBar />
+      <img className="preamble--pattern-left" src={PatternLeft} alt="pattern_left" />
       <MainPage />
     </div>
   );
